63979eeffce81ba06f2c3488feb7d7cc47bc28c9
Forbes Lindesay <fpfl2@cam.ac.uk>
2012-11-24 11:51:33 +0000

    Fix casing to work with npm on unix

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-exports['2D'] = require('simplify-2D');
-exports['3D'] = require('simplify-3D');
+try {
+  exports['2D'] = require('simplify-2D');
+  exports['3D'] = require('simplify-3D');
+} catch (ex) {
+  exports['2D'] = require('simplify-2d');
+  exports['3D'] = require('simplify-3d');
+}
